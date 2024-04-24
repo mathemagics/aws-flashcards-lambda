d508d1719946ec132780acf10b952afaa416e4c2
Paul Bodnar <higgsbison@gmail.com>
2024-04-23 22:08:44 -0600

    update to use better structure

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { AttributeValue, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import csv from 'csv-parser';
 import { Handler } from 'aws-lambda';
 import {Writable, Readable} from 'stream';
 import { finished } from 'stream/promises';
+import { v4 as uuidv4 } from 'uuid';
 
 const tableName = process.env.TABLE_NAME;
 
 const s3Client = new S3Client({ region: 'us-east-2'});
 const dynamoDbClient = new DynamoDBClient({ region: 'us-east-2'});
 
-type Flashcard = {key: string, value: string};
+type Flashcard = {id: string; q: string, a: string};
 
 const parseKey = (key: string) => {
     const keyParts = key.split('/');
@@ -25,13 +26,15 @@ export const handler: Handler = async (event, context) => {
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
     const {partitionKey, sectionName} = parseKey(key);
+
     const params = {
         Bucket: bucket,
         Key: key,
     };
 
-    const flashcards: Flashcard[] = [];
+    const flashcards: AttributeValue[] = [];
 
     try {
         const command = new GetObjectCommand(params);
@@ -46,7 +49,7 @@ export const handler: Handler = async (event, context) => {
             }
         });
 
-       (Body as Readable).pipe(csv()).pipe(outputStream);
+       (Body as Readable).pipe(csv({headers: ['question', 'answer']})).pipe(outputStream);
         await finished(outputStream);
     } catch (err) {
         const message = `Error getting object ${key} from bucket ${bucket}.`;
@@ -54,12 +57,22 @@ export const handler: Handler = async (event, context) => {
     }
 
     try {
+        const flashcardList = flashcards.map((row) => {
+            return {
+                M: {
+                    id: { S: uuidv4() },
+                    q: { S: Object.keys(row)[0] },
+                    a: { S: Object.values(row)[0] },
+                    row: { S: JSON.stringify(row) }
+                }
+            };
+        });
         const res = await dynamoDbClient.send(new PutItemCommand({
             TableName: tableName,
             Item: {
                 cert_id: { S: partitionKey },
                 section_id: { S: sectionName },
-                flashcards: { S: JSON.stringify(flashcards) },
+                flashcards: { L: flashcardList },
             }
         }));
         return res
